4f0045062b9654f786624c354a6dbeb928f7e789
jteo0027 <jteo0027@student.monash.edu>
2023-09-12 21:26:36 +0800

    modify the color background, delete the css code

--- static/status.js
+++ static/status.js
@@ -10,42 +10,15 @@ function changeColor(e) {
   }
 }
 
-// Get the "Priority" dropdown element by its ID
-const priorityDropdown = document.getElementById('priority_level');
-
-// Function to change the class of the "Priority" dropdown based on the selected option
-function changePriorityColor() {
-    const selectedOption = priorityDropdown.options[priorityDropdown.selectedIndex];
-    const priorityValue = selectedOption.value;
-
-    // Remove any existing priority class
-    priorityDropdown.className = '';
-
-    // Add the appropriate priority class based on the selected value
-    switch (priorityValue) {
-        case '1':
-            priorityDropdown.classList.add('low-priority');
-            break;
-        case '2':
-            priorityDropdown.classList.add('medium-priority');
-            break;
-        case '3':
-            priorityDropdown.classList.add('important-priority');
-            break;
-        case '4':
-            priorityDropdown.classList.add('urgent-priority');
-            break;
-        default:
-            // Handle the default case here (if needed)
-            break;
-    }
+// status box change color
+function changeColor2(e) {
+  if (e.value === 1) {
+    e.style.backgroundColor = 'green'; // Change to your desired color
+  } else if (e.value === 2) {
+    e.style.backgroundColor = 'yellow'; // Change to your desired color
+  } else if (e.value === 3) {
+    e.style.backgroundColor = 'orange'; // Change to your desired color
+  } else if (e.value === 4) {
+    e.style.backgroundColor = 'red'; // Change to your desired color
+  }
 }
-
-// Add an event listener to the "Priority" dropdown to call the function when it changes
-priorityDropdown.addEventListener('change', changePriorityColor);
-
-// Call the function initially to set the initial color based on the default selected option
-changePriorityColor();
-
-
-
