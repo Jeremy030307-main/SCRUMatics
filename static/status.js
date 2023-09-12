05d087e12141a0aa2854bcd0634c08b5c7314ff1
jteo0027 <jteo0027@student.monash.edu>
2023-09-12 23:56:05 +0800

    complete priority color

--- static/status.js
+++ static/status.js
@@ -10,15 +10,38 @@ function changeColor(e) {
   }
 }
 
+// Get all the table cells by their class or ID
+const cells = document.querySelectorAll('.custom-table td');
+
+// Loop through each cell and change the background color based on content
+cells.forEach(cell => {
+    const content = cell.textContent.toLowerCase();
+        
+    // Example: Change the color for specific content
+    if (content.includes(4)) {
+        cell.style.backgroundColor = 'red';
+        cell.style.color = 'white';
+    } else if (content.includes(3)) {
+        cell.style.backgroundColor = 'orange';
+    } else if (content.includes(2)) {
+        cell.style.backgroundColor = 'yellow';
+    } else if (content.includes(1)) {
+        cell.style.backgroundColor = 'green';
+    }
+});
+
 // status box change color
-function changeColor2(e) {
-  if (e.value === 1) {
-    e.style.backgroundColor = 'green'; // Change to your desired color
-  } else if (e.value === 2) {
-    e.style.backgroundColor = 'yellow'; // Change to your desired color
-  } else if (e.value === 3) {
-    e.style.backgroundColor = 'orange'; // Change to your desired color
-  } else if (e.value === 4) {
-    e.style.backgroundColor = 'red'; // Change to your desired color
-  }
-}
+// function changeColor2(e) {
+//   if (e.value === '1') {
+//     e.style.backgroundColor = 'green'; // Change to your desired color
+//   } else if (e.value === '2') {
+//     e.style.backgroundColor = 'yellow'; // Change to your desired color
+//   } else if (e.value === '3') {
+//     e.style.backgroundColor = 'orange'; // Change to your desired color
+//   } else if (e.value === '4') {
+//     e.style.backgroundColor = 'red'; // Change to your desired color
+//   }
+//   else {
+//     e.style.backgroundColor = 'white';
+//   }
+// }
